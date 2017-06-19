c813eaacdca6b4fb70973377d56ccd1141e21793
kintaiW <cscroll@foxmail.com>
2017-06-19 23:11:55 +0800

    add model using demo

--- Gemfile
+++ Gemfile
@@ -4,3 +4,13 @@ gem 'sinatra'
 gem 'chinese_pinyin'
 gem 'json'
 gem 'rdiscount'
+gem 'activerecord'
+
+
+group :development, :test do
+    gem 'sqlite3'
+end
+
+group :production do
+    #gem 'pg', '0.18.4'
+end
--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require 'sinatra'
+require 'active_record'
+require 'sqlite3'
+
+ActiveRecord::Base.establish_connection(    
+    :adapter=>'sqlite3',    
+    :database=>'data.sqlite3',    
+    :pool=>5,    
+    :timeout=>5000    
+    )  
+
+class CreateUsers < ActiveRecord::Migration[5.0]   
+  def change    
+    create_table :users do |t|    
+      t.string :name    
+      t.integer :age    
+    end   
+  end    
+end    
+
+CreateUsers.new.change
 
 get "/" do
     send_file "views/login.html"
@@ -7,3 +27,14 @@ end
 post "/new_user" do
   params.to_s
 end
+
+class User < ActiveRecord::Base
+end
+
+User.create name: "csc", age: 12
+
+get "/users" do
+  @users = User.all
+  @users.to_json
+end
+
